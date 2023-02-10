5fc15ff693da10295b3d7df6893e971288758a2b
ssah0002 <sipra.sahu@synchronoss.com>
2023-02-10 12:27:48 +0530

    res

---
+++ src/main/resources/com/TP/ASOG65NONOBF8/variables.java
@@ -0,0 +1,63 @@
+package com.TP.ASOG65NONOBF8;
+
+public class variables implements java.io.Serializable {
+static final long serialVersionUID = 1L;
+public variables(){}
+private com.wisor.brms.asog.SvcType svcTyp;
+private java.lang.String modifiedXml;
+private java.lang.String verizonRules;
+private java.lang.String flagFields;
+private java.lang.String flagIcsc;
+private java.lang.String flagCombination;
+private java.lang.String sendCheck;
+private java.lang.String multiCheck;
+public com.wisor.brms.asog.SvcType getSvcTyp() {
+return svcTyp;
+}
+public void setSvcTyp(com.wisor.brms.asog.SvcType svcTyp){
+this.svcTyp=svcTyp;
+}
+public java.lang.String getVerizonRules() {
+return verizonRules;
+}
+public void setVerizonRules(java.lang.String verizonRules){
+this.verizonRules=verizonRules;
+}
+public java.lang.String getMultiCheck() {
+return multiCheck;
+}
+public void setMultiCheck(java.lang.String multiCheck){
+this.multiCheck=multiCheck;
+}
+public java.lang.String getFlagFields() {
+return flagFields;
+}
+public void setFlagFields(java.lang.String flagFields){
+this.flagFields=flagFields;
+}
+public java.lang.String getFlagCombination() {
+return flagCombination;
+}
+public void setFlagCombination(java.lang.String flagCombination){
+this.flagCombination=flagCombination;
+}
+public java.lang.String getSendCheck() {
+return sendCheck;
+}
+public void setSendCheck(java.lang.String sendCheck){
+this.sendCheck=sendCheck;
+}
+public java.lang.String getModifiedXml() {
+return modifiedXml;
+}
+public void setModifiedXml(java.lang.String modifiedXml){
+this.modifiedXml=modifiedXml;
+}
+public java.lang.String getFlagIcsc() {
+return flagIcsc;
+}
+public void setFlagIcsc(java.lang.String flagIcsc){
+this.flagIcsc=flagIcsc;
+}
+
+}
